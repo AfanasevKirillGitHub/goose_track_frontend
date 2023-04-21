5b010fdd79767f64d4646d0123011369bf7fddec
LiubovErm <ermakovaluba27@gmail.com>
2023-04-21 17:30:41 +0300

    fix header

--- src/components/HomePage/HomePageHeader.jsx
+++ src/components/HomePage/HomePageHeader.jsx
@@ -1,15 +1,25 @@
 import * as SC from './HomePage.styled';
 import goose from '../../images/goose.png';
+import { useTranslation } from 'react-i18next';
+import { authPages } from '../../helpers/pages';
 import { MdLogin } from 'react-icons/md';
 
 export const HomePageHeader = () => {
+  const { t } = useTranslation();
+
   return (
     <SC.Header>
-      <SC.LogoImg src={goose} alt="logo goose"/>
+      <SC.LogoImg src={goose} alt="logo goose" />
       <SC.Title> Goose Track </SC.Title>
       <SC.ButtonList>
-        <SC.ButtonReg to="/registration">Sign up</SC.ButtonReg>
-        <SC.ButtonLogin to="/login">Login <MdLogin/></SC.ButtonLogin>
+        {authPages.map(({ href, name, id }) => (
+          <SC.NavListItem key={id}>
+            <SC.Button to={href}>
+              {t(`navigation.${name}`)}
+              <MdLogin />
+            </SC.Button>
+          </SC.NavListItem>
+        ))}
       </SC.ButtonList>
     </SC.Header>
   );
