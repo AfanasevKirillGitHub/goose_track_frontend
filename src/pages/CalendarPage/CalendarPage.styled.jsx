b199585f58d91082488d04a515ab39a1ab9cff09
SPoluliakh <S_Poluliakh@ukr.net>
2023-04-21 20:57:37 +0300

    add calendat

--- src/pages/CalendarPage/CalendarPage.styled.jsx
+++ src/pages/CalendarPage/CalendarPage.styled.jsx
@@ -9,18 +9,3 @@ export const ShadowWrapper = styled.div`
   border-bottom: 2px solid #464648;
   box-shadow: 0 0 0 1px #1a1a1a 0 8px 20px 6px #888;
 `;
-
-// export const Container = styled.div`
-//   display: flex;
-//   align-items: center;
-//   flex-direction: column;
-//   max-width: 320px;
-//   margin-left: auto;
-//   margin-right: auto;
-//   @media screen and (min-width: 768px) and (max-width: 1279px) {
-//     max-width: 768px;
-//   }
-//   @media screen and (min-width: 1280px) {
-//     max-width: 1280px;
-//   }
-// `;
