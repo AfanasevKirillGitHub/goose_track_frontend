dd9b532c91c6d8a1f91e47496c55e7ae51a26bea
AfanasevKirillGitHub <106386403+AfanasevKirillGitHub@users.noreply.github.com>
2023-04-22 16:09:49 +0000

    Deploying to gh-pages from @ AfanasevKirillGitHub/goose_track_frontend@a6fa7d89bb0d213060387695bfa4ce076d93b01c 🚀

---
+++ static/js/48.dfbfa53a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoose_track_frontend=self.webpackChunkgoose_track_frontend||[]).push([[48],{8048:function(n,e,c){c.r(e),c.d(e,{AccountPage:function(){return t}});var r=c(3329),t=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("p",{children:"Account Page"})})}}}]);
+//# sourceMappingURL=48.dfbfa53a.chunk.js.map
