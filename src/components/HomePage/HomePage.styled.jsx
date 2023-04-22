189f99d4d2f6a72f47329f7a3726c9545b80fb43
LiubovErm <105315327+LiubovErm@users.noreply.github.com>
2023-04-22 13:55:46 +0300

    add home page list

--- src/components/HomePage/HomePage.styled.jsx
+++ src/components/HomePage/HomePage.styled.jsx
@@ -46,7 +46,19 @@ export const ButtonList = styled.ul`
   }
 `;
 
-export const NavListItem = styled.li``;
+export const NavListItem = styled.li`
+  @media screen and (max-width: 768px) {
+    &:not(:last-child) {
+      margin-top: 32px;
+      margin-bottom: 200px;
+    }
+  }
+  @media screen and (min-width: 769px) {
+    margin-bottom: 0;
+    margin-top: 0;
+    margin-right: ${props => props.theme.space[4]}px;
+  }
+`;
 
 export const Button = styled(NavLink)`
   text-decoration: none;
@@ -58,9 +70,6 @@ export const Button = styled(NavLink)`
   justify-content: center;
   border-radius: ${props => props.theme.radii.medium};
   border: ${props => props.theme.borders.none};
-  margin-right: ${props => props.theme.space[4]}px;
-  margin-bottom: 200px;
-  margin-top: 32px;
   color: ${props => (props.to === '/login' ? '#3E85F3' : '#FFFFFF')};
   font-size: ${props => props.theme.fontSizes.s};
   font-weight: ${props => props.theme.fontWeights.semiBold};
@@ -72,6 +81,7 @@ export const Button = styled(NavLink)`
       props.to === '/login' ? '#3E85F3' : '#FFFFFF'};
     color: ${props => (props.to === '/login' ? '#FFFFFF' : '#3E85F3')};
     box-shadow: 0 0 15px rgba(236, 236, 236, 0.5);
+    text-decoration: underline;
   }
   svg {
     margin-left: ${props => props.theme.space[3]}px;
@@ -80,11 +90,92 @@ export const Button = styled(NavLink)`
   @media screen and (min-width: 768px) {
     width: 121px;
     height: 49px;
-    margin-bottom: 0;
-    margin-top: 0;
   }
 `;
 
-export const HomePageList = styled.li`
+export const HomePageList = styled.ul`
   list-style: none;
+  display: flex;
+  flex-direction: column;
+  background-color: ${props => props.theme.colors.white};
+  margin-bottom: 64px;
+`;
+
+export const ListItem = styled.li`
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 64px;
+  align-items:center;
+  @media screen and (min-width: 375px) {
+    width: 335px;
+  }
+  @media screen and (min-width: 768px) {
+    width: 704px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 1107px;
+    display:flex;
+    justify-content:space-between;
+  }
+`;
+
+export const ItemWrapper = styled.div`
+  @media screen and (min-width: 768px) {
+    width: 275px;
+  }
+`;
+
+export const ItemNumber = styled.p`
+  font-size: ${props => props.theme.fontSizes.homePageNumber};
+  font-weight: ${props => props.theme.fontWeights.bold};
+  color: ${props => props.theme.colors.blue};
+  @media screen and (max-width: 1439px) {
+    margin-top: 64px;
+  }
+`;
+
+export const ItemTitle = styled.h2`
+  display: inline;
+  text-transform: uppercase;
+  font-size: ${props => props.theme.fontSizes.bxl};
+  font-weight: ${props => props.theme.fontWeights.bold};
+  color: ${props => props.theme.colors.blue};
+  line-height: ${props => props.theme.lineHeights.l};
+  background-color: ${props => props.theme.colors.blue2};
+  border-radius: ${props => props.theme.radii.big2};
+  padding: 8px 18px;
+`;
+
+export const ItemSubtitle = styled.p`
+  font-size: ${props => props.theme.fontSizes.bxl};
+  font-weight: ${props => props.theme.fontWeights.bold};
+  color: ${props => props.theme.colors.black4};
+  line-height: ${props => props.theme.lineHeights.l};
+  @media screen and (max-width: 767px) {
+    margin-top: 8px;
+  }
+  @media screen and (min-width: 768px) {
+    margin-top: 14px;
+  }
+`;
+
+export const ItemText = styled.p`
+  font-size: ${props => props.theme.fontSizes.s};
+  font-weight: ${props => props.theme.fontWeights.medium};
+  line-height: ${props => props.theme.lineHeights.xl};
+  @media screen and (max-width: 767px) {
+    margin-top: 14px;
+  }
+  @media screen and (min-width: 768px) {
+    margin-top: 24px;
+  }
+`;
+
+export const ItemImg = styled.img`
+  @media screen and (max-width: 767px) {
+    margin-top: 40px;
+  }
+  @media screen and (min-width: 768px) and (max-width: 1439px) {
+    margin-top: 48px;
+  }
 `;
