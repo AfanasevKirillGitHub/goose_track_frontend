9ee45a2039c1da4cdf42411065e9db21f03939bf
AfanasevKirillGitHub <afanasev.dp@gmail.com>
2023-04-23 21:05:21 +0300

    header add pop-up, avatar

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 export const Header = styled.header`
   display: flex;
   justify-content: space-between;
-  width: 100%;
-  padding-top: 46px;
-  padding-bottom: 45px;
+  align-items: center;
+  width: 100vh;
+  padding: 46px 32px 35px 32px;
 `;
 export const HeaderTitle = styled.h1`
   font-family: 'Inter';
@@ -14,4 +14,10 @@ export const HeaderTitle = styled.h1`
   font-size: 32px;
   line-height: 32px;
   color: #111;
+`;
+
+export const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 14px;
 `;
--- src/components/Sidebar/LogoutBtn/LogoutBtn.styled.jsx
+++ src/components/Sidebar/LogoutBtn/LogoutBtn.styled.jsx
@@ -4,19 +4,22 @@ export const Button = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 131px;
-  height: 46px;
+  margin-left: ${props =>
+    props.design ? props.theme.space[4] : props.theme.space[0]}px;
+  width: ${props => (props.design ? '108' : '141')}px;
+  height: ${props => (props.design ? '37' : '46')}px;
   padding: ${props => props.theme.space[0]}px;
   border-radius: ${props => props.theme.radii.medium};
   border: ${props => props.theme.borders.none};
   color: ${props => (props.to === '/login' ? '#3E85F3' : '#FFFFFF')};
   background-color: ${props => (props.to === '/login' ? '#FFFFFF' : '#3E85F3')};
   cursor: pointer;
-  font-size: ${props => props.theme.fontSizes.l};
+  font-size: ${props => (props.design ? '14px' : props.theme.fontSizes.l)};
   line-height: ${props => props.theme.lineHeights.xl};
   font-weight: ${props => props.theme.fontWeights.semiBold};
   box-shadow: 0 0 15px rgba(236, 236, 236, 0.5);
-  transition: background-color 0.25s ease-in-out, color 0.25s ease-in-out, transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  transition: background-color 0.25s ease-in-out, color 0.25s ease-in-out,
+    transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
   svg {
     margin-left: ${props => props.theme.space[3]}px;
@@ -28,9 +31,9 @@ export const Button = styled.button`
     background-color: gold;
     transform: scale(1.1);
   }
-  
+
   @media screen and (min-width: 768px) {
-    width: 141px;
-    height: 56px;
+    width: ${props => (props.design ? '108' : '141')}px;
+    height: ${props => (props.design ? '37' : '46')}px;
   }
 `;
