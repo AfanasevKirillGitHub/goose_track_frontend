695045348b7caada5f15f37f3c459b9431f5705d
LiubovErm <105315327+LiubovErm@users.noreply.github.com>
2023-04-23 13:40:16 +0300

    fix

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,5 +1,4 @@
 import { RegisterForm } from '../../components/RegisterForm/RegisterForm/RegisterForm';
-import * as SC from './RegisterPage.styled';
 
 export interface IFormOneData {
   email: string;
