02351568fea8dc3c94ed5fa58cf74226d010b579
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-04-24 00:37:54 +0300

    add basic week style

--- src/pages/CurrentDayPage/CurrentDayPage.styled.ts
+++ src/pages/CurrentDayPage/CurrentDayPage.styled.ts
@@ -13,17 +13,26 @@ export const PageWrapper = styled.div`
 `;
 
 export const CellWrapper = styled.div<ICellWrapperProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   /* background-color: ${p => (p.isWeekend ? '#272829' : '#1e1f21')}; */
   color: black;
   ${p => p.isHeader && 'text-align:center'}
+  width: 27px;
+  height: 26px;
+
+  font-weight: 700;
+  color: #343434;
 `;
 
 export const CurrentDay = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: red;
-  border-radius: 50%;
+  background-color: ${props => props.theme.colors.blue};
+  color: ${props => props.theme.colors.white};
+  border-radius: ${props => props.theme.radii.normal};
 
   width: 100%;
   height: 100%;
@@ -32,20 +41,34 @@ export const CurrentDay = styled.div`
 export const WeekWrapper = styled.div`
   display: flex;
   /* align-items: center; */
-  justify-content: space-around;
+  justify-content: space-between;
+  width: 100%;
+  border: 1px solid rgba(220, 227, 229, 0.8);
+  border-radius: 8px;
+  padding: 10px 46px;
+
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 1.286;
+  color: #616161;
 `;
 
-export const DaysWrapper = styled.div`
-  display: flex;
-  /* align-items: center; */
-  justify-content: space-around;
-`;
+// export const DaysWrapper = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-around;
+// `;
 
-export const DayOfWeek = styled.div``;
-
-export const WeekAndDayWrapper = styled.div`
+export const DayOfWeek = styled.div`
   display: flex;
   flex-direction: column;
-  /* justify-content: space-around; */
-  width: 100%;
+  align-items: center;
+  gap: 4px;
 `;
+
+// export const WeekAndDayWrapper = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-around;
+//   width: 100%;
+// `;
