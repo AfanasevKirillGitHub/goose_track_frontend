ddcffb96a1856099d51246b994425864c9310d04
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2023-04-26 22:26:43 +0300

    fix style position of logout button

--- src/pages/UserPage/UserPage.styled.ts
+++ src/pages/UserPage/UserPage.styled.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+  min-height: 100vh;
+
   @media screen and (min-width: 1440px) {
     display: grid;
     grid-template-columns: 289px 1fr;
