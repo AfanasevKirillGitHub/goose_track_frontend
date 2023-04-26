06e84b95688b8b15a0ac26beeb98571b2e62c1ad
AfanasevKirillGitHub <afanasev.dp@gmail.com>
2023-04-26 23:32:47 +0300

    add style modal and header

--- src/components/Header/UserAvatar/UserAvatar.jsx
+++ src/components/Header/UserAvatar/UserAvatar.jsx
@@ -3,17 +3,17 @@ import { useAuth } from '../../../hooks/useAuth';
 import avatarDefault from '../../../images/avatar-default.png';
 import * as SC from './UserAvatar.styled';
 
-export const UserAvatar = () => {
+export const UserAvatar = ({ invert }) => {
   const { user } = useAuth();
   const { t } = useTranslation();
 
   return (
-    <SC.Wrapper>
+    <SC.Wrapper invert={invert}>
       <SC.Text>{t(`${user.name}`)}</SC.Text>
       {user.avatarURL !== null ? (
-        <SC.UserAvatar>
+        <SC.AvatarUser>
           <SC.Avatar src={user.avatarURL} alt="User Avatar" />
-        </SC.UserAvatar>
+        </SC.AvatarUser>
       ) : (
         <SC.AvatarDefault>
           <SC.Avatar src={avatarDefault} alt="Default Avatar" />
