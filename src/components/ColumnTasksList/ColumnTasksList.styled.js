7f05232ff4707c263623bed58fcd6f9a764c015a
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-04-27 16:21:24 +0300

    current day fix

--- src/components/ColumnTasksList/ColumnTasksList.styled.js
+++ src/components/ColumnTasksList/ColumnTasksList.styled.js
@@ -4,12 +4,15 @@ export const TaskList = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 14px;
+  max-height: 335px;
+  overflow: hidden;
+  overflow-y: auto;
 
-  margin-bottom: 0;
+  margin-bottom: 8;
 
   @media screen and (min-width: 768px) {
     gap: 18px;
-
+    max-height: 378px;
     margin-bottom: ${p => p.theme.space[5]}px;
 
     background-color: white;
