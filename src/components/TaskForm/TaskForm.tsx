6059fa9687383e1621c5ac6f8f2f2e04ca0cd1fb
SPoluliakh <S_Poluliakh@ukr.net>
2023-04-30 13:09:03 +0300

    fix

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -15,9 +15,24 @@ import { PRIORITY } from '../../helpers/enums';
 import { t } from 'i18next';
 import { ITask } from '../../helpers/interfaces/taskApiInterface/taskApiInterface';
 
+interface IFieldsData {
+  date?: string;
+  title?: string;
+  start?: string;
+  end?: string;
+  priority?: string;
+  status: string;
+  _id?: string;
+  owner?: {
+    email: string;
+    name: string;
+    _id: string;
+  };
+}
+
 interface IProps {
   toggleModal: () => void;
-  fieldsData: ITask;
+  fieldsData: IFieldsData | ITask;
 }
 
 export const TaskForm = ({ fieldsData, toggleModal }: IProps) => {
@@ -35,7 +50,7 @@ export const TaskForm = ({ fieldsData, toggleModal }: IProps) => {
   //js
   const lang = localStorage.getItem('i18nextLng');
 
-  const modalType = fieldsData.title ? `edit` : `add`;
+  const modalType = (fieldsData.title as string) ? `edit` : `add`;
 
   const taskCreateTime = fieldsData?.start
     ? fieldsData.start
--- src/components/TaskModal/TaskModal.tsx
+++ src/components/TaskModal/TaskModal.tsx
@@ -2,14 +2,34 @@ import { ITask } from '../../helpers/interfaces/taskApiInterface/taskApiInterfac
 import { Modal } from '../Modal/Modal';
 import { TaskForm } from '../TaskForm';
 
+interface IFieldsData {
+  date?: string;
+  title?: string;
+  start?: string;
+  end?: string;
+  priority?: string;
+  status: string;
+  _id?: string;
+  owner?: {
+    email: string;
+    name: string;
+    _id: string;
+  };
+}
+
 interface IProps {
   status: string;
-  data: ITask;
+  data: ITask | null;
   toggleModal: () => void;
 }
 
 export const TaskModal = ({ status, data, toggleModal }: IProps) => {
-  const formData = { ...data, status };
+  let formData: IFieldsData;
+  if (!data) {
+    formData = { status };
+  } else {
+    formData = { ...data, status };
+  }
 
   return (
     <Modal toggleModal={toggleModal}>
