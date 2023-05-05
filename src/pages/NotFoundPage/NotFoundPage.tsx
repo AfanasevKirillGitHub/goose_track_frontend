8973cf59dae20bfb0e5c92bd3adc8a2319664b33
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-05-05 15:40:52 +0300

    add link to petly

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,15 +1,26 @@
-import * as SC from './NotFoundPage.styled'
-import { SVG } from "../../images";
+import { useTranslation } from 'react-i18next';
+import * as SC from './NotFoundPage.styled';
+import { SVG } from '../../images';
 
 export const NotFoundPage = () => {
+  const { t } = useTranslation();
+
   return (
-    //   <SVG.NotFoundIcon width="240" height="240" />
-      <SC.NotFoundPage>
-          <SC.Wrapper>
-      <SVG.NotFoundIcon/>
-          </SC.Wrapper>
-          <SC.Text>Try starting from the home page</SC.Text>
-          <SC.Button type='button' to={'/'}>To home page</SC.Button>
-      </SC.NotFoundPage>
+    <SC.NotFoundPage>
+      <SC.Wrapper>
+        <SVG.NotFoundIcon />
+      </SC.Wrapper>
+      <SC.Text>{t('try home')}</SC.Text>
+      <SC.Button type="button" to={'/'}>
+        {t('home')}
+      </SC.Button>
+      <SC.PetlyLink
+        href="https://afanasevkirillgithub.github.io/petly_frontend/home"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {t('find')}
+      </SC.PetlyLink>
+    </SC.NotFoundPage>
   );
-}
+};
